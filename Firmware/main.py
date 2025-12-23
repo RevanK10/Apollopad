1ca6f097bf735bfc1c440f16da7c5f9a24a3d448
RevanK10 <162674927+RevanK10@users.noreply.github.com>
2025-12-23 14:47:51 -0500

    Update main.py

--- Firmware/main.py
+++ Firmware/main.py
@@ -1,55 +1,41 @@
+# Import board pin definitions
 import board
 
+# KMK core
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
+
+# Encoder support
 from kmk.modules.encoder import EncoderHandler
 
-# ----------------------------
-# Keyboard object
-# ----------------------------
+# Create keyboard object
 keyboard = KMKKeyboard()
 
-# ----------------------------
-# Keys (5 switches + encoder button)
-# ----------------------------
+# ========== KEYS ==========
+# Order MUST match PCB wiring order
+
 PINS = [
-    board.A0,  # SW1
-    board.A1,  # SW2
-    board.A2,  # SW3
-    board.A3,  # SW4
-    board.D6,  # SW5
-    board.D7,  # Encoder push
+    board.A0,  # SW1 - Copy
+    board.A1,  # SW2 - Paste
+    board.A2,  # SW3 - Cut
+    board.A3,  # SW4 - Play / Pause
+    board.D6,  # SW5 - Screenshot
+    board.D7,  # Encoder press (Mute)
 ]
 
 keyboard.matrix = KeysScanner(
     pins=PINS,
-    value_when_pressed=False,  # internal pull-ups
+    value_when_pressed=False,
 )
 
-# ----------------------------
-# Rotary encoder
-# ----------------------------
+# ========== ROTARY ENCODER ==========
 encoder = EncoderHandler()
 keyboard.modules.append(encoder)
 
 encoder.pins = (
-    (board.D1, board.D2),  # A, B
+    (board.D2, board.D1),  # Encoder A, B
 )
-
-# ----------------------------
-# Keymap
-# ----------------------------
-keyboard.keymap = [
-    [
-        KC.MUTE,        # SW1
-        KC.VOLDOWN,     # SW2
-        KC.VOLUP,       # SW3
-        KC.PLAY_PAUSE,  # SW4
-        KC.ENTER,       # SW5
-        KC.MUTE,        # Encoder button
-    ]
-]
 
 encoder.map = [
     (
@@ -58,8 +44,17 @@ encoder.map = [
     )
 ]
 
-# ----------------------------
-# Start KMK
-# ----------------------------
+# ========== KEYMAP ==========
+keyboard.keymap = [
+    [
+        KC.COPY,        # SW1
+        KC.PASTE,       # SW2
+        KC.CUT,         # SW3
+        KC.PLAY_PAUSE,  # SW4
+        KC.SCREENSHOT,  # SW5
+        KC.MUTE,        # Encoder press
+    ]
+]
+
 if __name__ == '__main__':
     keyboard.go()
